e5b80f8bb61b51fe275040b9e13004cf50a9d166
Borisova Polina <borisova.p@otr.ru>
2020-10-01 20:50:14 +0300

    Bubble sort implementation

---
+++ src/com/company/BubbleSort.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class BubbleSort {
+
+    public static int[] doBubbleSort(int[] array) {
+
+        for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0;
+             lastUnsortedIndex--) {
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i + 1);
+                }
+            }
+        }
+
+        return array;
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int[] array = {20, -11, 43, 2, -22, 9};
+
+        BubbleSort.doBubbleSort(array);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
+        }
     }
 }
